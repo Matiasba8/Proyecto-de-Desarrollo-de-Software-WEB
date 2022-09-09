79f803b9a33cb535341863b2da74f1cfe838402f
matiasba8 <matias.bustos@redcomercio.cl>
2022-09-08 20:22:46 -0400

    ignore jsdom?

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+plugins: [
+    new webpack.IgnorePlugin(/jsdom$/)
+]
+
 module.exports = environment.toWebpackConfig()
