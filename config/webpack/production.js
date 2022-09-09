9dc02a9a4d6b8a0063ccd921279f1903e47bd2a4
matiasba8 <matias.bustos@redcomercio.cl>
2022-09-08 20:27:31 -0400

    delete this

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,8 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-plugins: [
-    new webpack.IgnorePlugin(/jsdom$/)
-]
-
 module.exports = environment.toWebpackConfig()
