44fca4a590c910f15a3be522a96353efaecd42ea
matiasba8 <matias.bustos@redcomercio.cl>
2022-09-11 23:08:15 -0300

    VectorForce Storage

--- app/javascript/packs/task.js
+++ app/javascript/packs/task.js
@@ -2,133 +2,11 @@
 
 
 const fabric = require("fabric").fabric;
-var id_counter = 0;
-var ids_to_delete = [];
-var objects = [];
-
-var data_to_send = []
-// Elimina un objecto por su id a objects
-function removeObjectFromObjects(id){
-    objects.some(function(object){
-        if (id === object["id"]){
-            objects.splice(objects.indexOf(object),1)
-            return true;
-        }
-    })
-}
 
 // -***- Render Objects -***-
 
-function renderApoyoFijo(canvas, scaleX, scaleY, top, left, angle ){
-    console.log("FIJO")
-    console.log(`id_counter: ${id_counter}`)
-
-    fabric.Image.fromURL('https://www.pngmart.com/files/4/Triangle-PNG-Clipart.png', (img) => {
-        img.set({
-            id: id_counter,
-            scaleX: scaleX,
-            scaleY: scaleY,
-            top: top,
-            left: left,
-            angle: angle,
-        });
-        img.scaleToWidth(30);
-        img.scaleToHeight(30);
-        canvas.add(img)
-    });
-
-    objects.push({
-        "id": id_counter,
-        "width": 0,
-        "height": 0,
-        "canvas_type": "image",
-        "object_type": "apoyo-fijo",
-        "force": 0,
-        "angle": 0,
-        "scaleX": scaleX,
-        "scaleY": scaleY,
-        "top": top,
-        "left": left
-    })
-}
-
-
-function renderApoyoDes(canvas, scaleX, scaleY, top, left, angle ){
-    console.log("DES")
-    console.log(`id_counter: ${id_counter}`)
-    fabric.Image.fromURL('https://www.pngmart.com/files/4/Triangle-PNG-Clipart.png', (img) => {
-        img.set({
-            id: id_counter,
-            scaleX: scaleX,
-            scaleY: scaleY,
-            top: top,
-            left: left,
-            angle: angle,
-        });
-        img.scaleToWidth(30);
-        img.scaleToHeight(30);
-        canvas.add(img)
-    });
-
-    objects.push({
-        "id": id_counter,
-        "width": 0,
-        "height": 0,
-        "canvas_type": "image",
-        "object_type": "apoyo-deslizante",
-        "force": 0,
-        "angle": angle,
-        "scaleX": scaleX,
-        "scaleY": scaleY,
-        "top": top,
-        "left": left
-    })
-}
-
-function renderEmpotramiento(canvas, scaleX, scaleY, top, left, angle ){
-    console.log("EMPOTRA")
-    console.log(`id_counter: ${id_counter}`)
-    fabric.Image.fromURL('https://www.pngmart.com/files/4/Triangle-PNG-Clipart.png', (img) => {
-        img.set({
-            id: id_counter,
-            scaleX: scaleX,
-            scaleY: scaleY,
-            top: top,
-            left: left,
-            angle: angle,
-        });
-        img.scaleToWidth(30);
-        img.scaleToHeight(30);
-        canvas.add(img)
-    });
-
-    objects.push({
-        "id": id_counter,
-        "width": 0,
-        "height": 0,
-        "canvas_type": "image",
-        "object_type": "empotramiento",
-        "force": 0,
-        "angle": angle,
-        "scaleX": scaleX,
-        "scaleY": scaleY,
-        "top": top,
-        "left": left
-    })
-}
-
-
-
-
-
-// -***-----------------------------------***-----
-
-
 function renderRectangle(canvas, left, top, width, height, angle, scaleX, scaleY) {
-    console.log("RECTR")
-    console.log(`id_counter: ${id_counter}`)
     var rect = new fabric.Rect({
-        id: id_counter,
         left: left,//90,
         top: top,//60,
         fill: '#999999',
@@ -142,30 +20,13 @@ function renderRectangle(canvas, left, top, width, height, angle, scaleX, scaleY
         scaleY: scaleY
     });
 
-    objects.push({
-        "id": id_counter,
-        "width": width,
-        "height": height,
-        "canvas_type": "rect",
-        "object_type": "bar",
-        "force": 0,
-        "angle": angle,
-        "scaleX": scaleX, //Cambiar al valor default
-        "scaleY": scaleY,
-        "top": top,//100,
-        "left": left//50
-    })
-
     canvas.add(rect);
     canvas.setActiveObject(rect);
     return rect;
 }
 
 function renderText(canvas, left, top, width, height, angle, scaleX, scaleY) {
-    console.log("TEXT")
-    console.log(`id_counter: ${id_counter}`)
     canvas.add(new fabric.IText('Tap and Type', {
-        id: id_counter,
         fontFamily: 'arial black',
         left: left,//50,
         top: top,//100 ,
@@ -175,28 +36,12 @@ function renderText(canvas, left, top, width, height, angle, scaleX, scaleY) {
         scaleX: scaleX,
         scaleY: scaleY,
     }));
-    objects.push({
-        "id": id_counter,
-        "width": width,
-        "height": height,
-        "canvas_type": "i-text",
-        "object_type": "text",
-        "force": 0,
-        "angle": angle,
-        "scaleX": scaleX, //Cambiar al valor default
-        "scaleY": scaleY,
-        "top": top,
-        "left": left
-    })
 }
 
 function renderVector(canvas, left, top, width, height, angle, scaleX, scaleY, force) {
-    console.log("VECTOR")
-    console.log(`id_counter: ${id_counter}`)
     fabric.Image.fromURL('https://cdn-icons-png.flaticon.com/512/664/664866.png', (img) => {
-
         img.set({
-            id: id_counter,
+            force: force,
             scaleX: scaleX,//1/4,
             scaleY: scaleY,//1/4,
             top: top,//100,
@@ -208,21 +53,54 @@ function renderVector(canvas, left, top, width, height, angle, scaleX, scaleY, f
 
         console.log(`Img_id: ${img.id}`)
     });
-    objects.push({
-        "id": id_counter,
-        "width": width,
-        "height": height,
-        "canvas_type": "image",
-        "object_type": "vector",
-        "force": force,
-        "angle": angle,
-        "scaleX": scaleX,
-        "scaleY": scaleY,
-        "top": top,
-        "left": left
-    })
 }
 
+function renderApoyoFijo(canvas, scaleX, scaleY, top, left, angle ){
+
+    fabric.Image.fromURL('https://www.pngmart.com/files/4/Triangle-PNG-Clipart.png', (img) => {
+        img.set({
+            scaleX: scaleX,
+            scaleY: scaleY,
+            top: top,
+            left: left,
+            angle: angle,
+        });
+        img.scaleToWidth(30);
+        img.scaleToHeight(30);
+        canvas.add(img)
+    });
+}
+
+function renderApoyoDes(canvas, scaleX, scaleY, top, left, angle ){
+    fabric.Image.fromURL('https://www.pngmart.com/files/4/Triangle-PNG-Clipart.png', (img) => {
+        img.set({
+            scaleX: scaleX,
+            scaleY: scaleY,
+            top: top,
+            left: left,
+            angle: angle,
+        });
+        img.scaleToWidth(30);
+        img.scaleToHeight(30);
+        canvas.add(img)
+    });
+}
+
+function renderEmpotramiento(canvas, scaleX, scaleY, top, left, angle ){
+    fabric.Image.fromURL('https://www.pngmart.com/files/4/Triangle-PNG-Clipart.png', (img) => {
+        img.set({
+            scaleX: scaleX,
+            scaleY: scaleY,
+            top: top,
+            left: left,
+            angle: angle,
+        });
+        img.scaleToWidth(30);
+        img.scaleToHeight(30);
+        canvas.add(img)
+    });
+}
+// -***-----------------------------------***-----
 
 function canvasGridSetUp(canvas){
     var grid = 30;
@@ -334,47 +212,69 @@ function canvasGridSetUp(canvas){
 
 function attachButtonsEvents(canvas){
     $('#addBar').on('click', function(){
-        id_counter += 1
         renderRectangle(canvas, 90, 60, 180, 30, 0, 1, 1);
     });
 
     $('#addText').on('click', function(){
-        id_counter += 1
         renderText(canvas,50, 100, 0, 0, 0, 1, 1);
     });
 
     $('#open-vector-modal').on('click', function(){
-        id_counter += 1
         $('#exampleModal').modal("toggle")
     });
 
     $('#addApoyoFijo').on('click', function(){
-        id_counter += 1
         renderApoyoFijo(canvas, 1/4, 1/4, 100, 50, 0)
     });
 
     $('#addApoyoDes').on('click', function(){
-        id_counter += 1
         renderApoyoDes(canvas, 1/4, 1/4, 100, 50, 0)
     });
 
     $('#addEmpotramiento').on('click', function(){
-        id_counter += 1
         renderEmpotramiento(canvas, 1/4, 1/4, 100, 50, 0)
     });
 
     $('#guardar').on('click', function(){
-        data_to_send.push(objects)
-        data_to_send.push({task_id: $('#task-id').val()})
+        var json = JSON.stringify(canvas.toJSON(['force']));
+        console.log(json);
         $.ajax({
             type: "POST",
-            url: "/save2",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(data_to_send),
+            url: $('#save-task').val(),
+            dataType: 'text',
+            data: {canvas_stringify: json, id: $('#task-id').val()},
             success: function (data) {
+                console.log(data);
             }
         });
+    });
 
+    // Al ingresar crear en el modal del vector se crea la imagen con los atributos correspondientes
+    $('#create-vector').on("click", function () {
+        var v_force = $('#vector-force').val();
+        var v_angle = $('#vector-angle').val();
+
+        fabric.Image.fromURL('https://cdn-icons-png.flaticon.com/512/664/664866.png', (img) => {
+            img.set({
+                scaleX: 1/4,
+                scaleY: 1/4,
+                top: 100,
+                left: 50,
+
+            });
+            img['force'] = v_force;
+            img.rotate(v_angle)
+            canvas.add(img)
+
+            console.log(`Img_id: ${img.id}`)
+        });
+        //Close modal
+        $('#exampleModal').modal("toggle");
+    })
+
+
+    $('.close-vector-modal').on("click", function(){
+        $('#exampleModal').modal("toggle");
     });
 }
 
@@ -384,10 +284,10 @@ function setUpDeleteIcon(){
         var target = transform.target;
         var canvas = target.canvas;
 
-        console.log(`item removido id: ${target.id}`)
+        //console.log(`item removido id: ${target.id}`)
 
-        removeObjectFromObjects(target.id)
-        ids_to_delete.push(target.id);
+        //removeObjectFromObjects(target.id)
+        //ids_to_delete.push(target.id);
 
         canvas.remove(target);
         canvas.requestRenderAll();
@@ -424,49 +324,30 @@ function setUpDeleteIcon(){
     return img
 }
 
-//Object Modified Event
-var onObjectModified = function(e) {
-/*
-    console.log(`scaleX: ${e.target.get("scaleX")}`)
-*/
-
-    alert(e.target.id);
-    objects.some(function(object){
-        if (e.target.id === object["id"]){
-            /*console.log(`Before: ${object["angle"]}`)
-            console.log(`Before: ${object["scaleX"]}`)
-            console.log(`Before: ${object["scaleY"]}`)
-            console.log(`Before: ${object["top"]}`)
-            console.log(`Before: ${object["left"]}`)*/
-            object["angle"] = e.target.angle
-            object["scaleX"] = e.target.scaleX
-            object["scaleY"] = e.target.scaleY
-            object["top"] = e.target.top
-            object["left"] = e.target.left
-            /*console.log(`after: ${object["id"]}`)
-            console.log(`after: ${object["scaleX"]}`)
-            console.log(`after: ${object["angle"]}`)
-            console.log(`after: ${object["scaleX"]}`)
-            console.log(`after: ${object["scaleY"]}`)
-            console.log(`after: ${object["top"]}`)
-            console.log(`after: ${object["left"]}`)*/
-        }
-    })
-
-};
-//Object Removed Event
-var onObjectRemoved = function (e){
-};
-
-
 $( document ).on('turbolinks:load', common_events)
 
 function common_events(){
 
     console.log("Common events")
 
-    //Load canvas from server
     $.ajax({
+        type: "POST",
+        url: $('#load-canvas').val(),
+        dataType: 'json',
+        data: {id: $('#task-id').val()},
+        success: function(data){
+            canvas.loadFromJSON(data, function(){
+                canvas.renderAll();
+                canvas.forEachObject(function(object){
+                    if (object.type == "line"){
+                        object.selectable = false
+                    }
+                })
+            })
+        }
+    })
+    //Load canvas from server
+    /* $.ajax({
         type: "POST",
         url: "/load2",
         dataType: 'json',
@@ -475,111 +356,41 @@ function common_events(){
             data.some(function (val, index) {
 
                 if (val.object_type === "bar"){
-                    id_counter += 1
-                    console.log("BAR")
-                    console.log(`id_counter: ${id_counter}`)
                     renderRectangle(canvas,val.left,val.top,val.width, val.height, val.angle, val.scaleX, val.scaleY)
                 }
                 else if (val.object_type === "vector"){
-                    id_counter += 1
-                    console.log("BAR")
-                    console.log(`id_counter: ${id_counter}`)
                     renderVector(canvas,val.left,val.top,val.width, val.height, val.angle, val.scaleX, val.scaleY, val.force)
                 }
                 else if (val.object_type === "text"){
-                    id_counter += 1
-                    console.log("text")
-                    console.log(`id_counter: ${id_counter}`)
                     renderText(canvas,val.left,val.top,val.width, val.height, val.angle, val.scaleX, val.scaleY)
                 }
                 else if (val.object_type === "apoyo-deslizante"){
-                    id_counter += 1
-                    console.log("deslizante")
-                    console.log(`id_counter: ${id_counter}`)
                     renderApoyoDes(canvas, val.scaleX, val.scaleY, val.top, val.left, val.angle)
                 }
                 else if (val.object_type === "apoyo-fijo"){
-                    id_counter += 1
-                    console.log("fijo")
-                    console.log(`id_counter: ${id_counter}`)
                     renderApoyoFijo(canvas, val.scaleX, val.scaleY, val.top, val.left, val.angle)
                 }
                 else if (val.object_type === "empotramiento"){
-                    id_counter += 1
-                    console.log("empotramiento")
-                    console.log(`id_counter: ${id_counter}`)
                     renderEmpotramiento(canvas, val.scaleX, val.scaleY, val.top, val.left, val.angle)
                 }
             })
         }
-    });
+    }); */
 
     console.log("It executes!");
 
     var canvas = this.__canvas = new fabric.Canvas('canvas');
     var json;
 
-
-
-    canvas.on('object:removed', onObjectRemoved);
-    canvas.on('object:modified', onObjectModified);
-
     canvas.setHeight(window.innerHeight);
     canvas.setWidth(window.innerWidth);
 
     canvasGridSetUp(canvas)
 
-    // canvas.on('object:scaling', function(options) {
-    //     options.target.set({
-    //       left: Math.round(options.target.left / grid) * grid,
-    //       top: Math.round(options.target.top / grid) * grid
-    //     });
-    // });
-
     var img = setUpDeleteIcon()
     attachButtonsEvents(canvas)
 
-    // Al ingresar crear en el modal del vector se crea la imagen con los atributos correspondientes
-    $('#create-vector').on("click", function () {
-        var v_force = $('#vector-force').val();
-        var v_angle = $('#vector-angle').val();
-
-        fabric.Image.fromURL('https://cdn-icons-png.flaticon.com/512/664/664866.png', (img) => {
-            img.set({
-                id: id_counter,
-                scaleX: 1/4,
-                scaleY: 1/4,
-                top: 100,
-                left: 50,
-
-            });
-            img.rotate(v_angle)
-            canvas.add(img)
-
-            console.log(`Img_id: ${img.id}`)
-        });
-        objects.push({
-            "id": id_counter,
-            "width": 0,
-            "height": 0,
-            "canvas_type": "image",
-            "object_type": "vector",
-            "force": v_force,
-            "angle": v_angle,
-            "scaleX": 1/4,
-            "scaleY": 1/4,
-            "top": 100,
-            "left": 50
-        })
-        //Close modal
-        $('#exampleModal').modal("toggle");
+    canvas.on('object:modified', function(e){
+        console.log(e.target.get('force'));
     })
-
-
-    $('.close-vector-modal').on("click", function(){
-        $('#exampleModal').modal("toggle");
-    });
-
-    console.log("test")
 }
-
