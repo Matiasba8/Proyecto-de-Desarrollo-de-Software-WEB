1baac667cf17f61d092e024c179828fbd7e89db7
matiasba8 <matias.bustos@redcomercio.cl>
2022-09-22 15:03:23 -0300

    feat(add student model)

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,6 +9,10 @@ class UserController < ApplicationController
   end
 
   def new
-    @new_user = Professor.create(email: params[:email], name: params[:name], phone_number: params[:phone_number], password: params[:password])
+    if params[:role] == "Profesor"
+      @new_professor = Professor.create(email: params[:email], name: params[:name], phone_number: params[:phone_number], password: params[:password])
+    elsif params[:role] == "Estudiante"
+      @new_professor = Student.create(email: params[:email], name: params[:name], phone_number: params[:phone_number], password: params[:password])
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_13_234623) do
+ActiveRecord::Schema.define(version: 2022_09_22_174954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2022_09_13_234623) do
     t.string "name"
     t.string "password"
     t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "phone_number"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
